55b2b983d2df74b763400f7ca1194fdf6fe323d3
Vilayat Safarov <vilayat571@gmail.com>
2024-07-22 08:41:28 -0700

    try

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/styles/index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import { Provider } from "react-redux";
-// import { store } from "./redux/store.ts";
 import App from "./pages/Main/App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
- 
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
