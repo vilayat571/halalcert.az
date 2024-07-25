db426f188bfce1302138a545576889324efefc07
Vilayat Safarov <vilayat571@gmail.com>
2024-07-24 18:21:57 -0700

    some changes

--- src/constants/Main/Linksofbenefits.ts
+++ src/constants/Main/Linksofbenefits.ts
@@ -10,21 +10,25 @@ export const linksofBenefits:ILinks[] = [
   {
     id: 1,
     href: "/",
+    alt:'prezident.az saytının loqosu - AzeSert MMC şirkəti',
     src: "https://static.president.az/assets/svg/gerb-721e7548761107d59f6c0daa8d942f65b82dfa61030d0fe4b237f5b9bb436d18.svg",
   },
   {
     id: 2,
     href: "/",
+    alt:'heydar-aliyev-foundation.org saytının loqosu - AzeSert MMC şirkəti',
     src: "https://heydar-aliyev-foundation.org/images/logo_az.png",
   },
   {
     id: 3,
     href: "/",
+    alt:'haydar aliyev markazi saytının loqosu - AzeSert MMC şirkəti',
     src: "https://mir-s3-cdn-cf.behance.net/projects/404/edf725103551139.Y3JvcCwzMTEzLDI0MzUsNTIzLDA.png",
   },
   {
     id: 4,
     href: "/",
+    alt:'mehriban-aliyeva saytının loqosu - AzeSert MMC şirkəti',
     src: "https://azerbaijan.az/media/images/banners/mehriban-aliyeva.jpg",
   },
 ];
