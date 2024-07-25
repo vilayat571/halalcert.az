cdc1a94d0a3adecc44bc99f38a983bd2c4bacc37
Vilayat Safarov <vilayat571@gmail.com>
2024-07-24 18:26:50 -0700

    font styling

--- src/constants/Main/Companies.ts
+++ src/constants/Main/Companies.ts
@@ -4,42 +4,50 @@ export const companiesLinks: ILinks[] = [
   {
     id: 1,
     href: "/",
+    alt:"azersun.com şirkətinin loqosu AZESERT MMC şirkətinin vebsaytı",
     src: "https://azersun.com/_ipx/w_20/logo.svg",
   },
   {
     id: 2,
     href: "/",
+    alt:"azersun.com şirkətinin loqosu AZESERT MMC şirkətinin vebsaytı",
     src: "https://sahliyali.com/uploads/settings/1801000191049108.png",
   },
   {
     id: 3,
     href: "/",
+    alt:"azersun.com şirkətinin loqosu AZESERT MMC şirkətinin vebsaytı",
     src: "https://www.cennet.az/wp-content/uploads/2019/04/logo.png",
   },
   {
     id: 4,
     href: "/",
+    alt:"azersun.com şirkətinin loqosu AZESERT MMC şirkətinin vebsaytı",
     src: "https://static.wixstatic.com/media/950cde_d911e3f367d04c7ebbc11625395d67d7~mv2.png/v1/fill/w_338,h_199,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/950cde_d911e3f367d04c7ebbc11625395d67d7~mv2.png",
   },
   {
     id: 5,
     href: "/",
+    alt:"azersun.com şirkətinin loqosu AZESERT MMC şirkətinin vebsaytı",
     src: "https://millitv.az/uploads/posts/2023-03/14.jpg",
   },
   {
     id: 6,
     href: "/",
+    alt:"azersun.com şirkətinin loqosu AZESERT MMC şirkətinin vebsaytı",
     src: "https://azprotein.az/uploads/2023/09/logo-1.png",
   },
 
  {
     id: 7,
     href: "/",
+    alt:"azersun.com şirkətinin loqosu AZESERT MMC şirkətinin vebsaytı",
     src: "https://saba.az/uploads/photos/setting/logo.svg",
   },
   {
     id: 8,
     href: "/",
+    alt:"azersun.com şirkətinin loqosu AZESERT MMC şirkətinin vebsaytı",
     src: "https://siyazantoyuq.az/assets/img/Logo.svg",
   },
 
